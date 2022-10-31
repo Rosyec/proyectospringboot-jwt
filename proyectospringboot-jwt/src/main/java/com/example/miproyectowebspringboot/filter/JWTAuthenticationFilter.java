6dbea0caa36e9572040dfa87ba90fbd55bfe11fa
Rosyec <chechomens@gmail.com>
2022-10-31 13:46:45 -0500

    Generando nuestro token

--- proyectospringboot-jwt/src/main/java/com/example/miproyectowebspringboot/filter/JWTAuthenticationFilter.java
+++ proyectospringboot-jwt/src/main/java/com/example/miproyectowebspringboot/filter/JWTAuthenticationFilter.java
@@ -1,5 +1,15 @@
 package com.example.miproyectowebspringboot.filter;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,10 +17,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    //Genera nuestra llave statica
+    public static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     private AuthenticationManager authenticationManager;
 
@@ -29,14 +51,46 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String password = obtainPassword(request);
         password = (password != null) ? password : "";
 
-        if (username!= null && password != null) {
+        if (username != null && password != null) {
             logger.info("Username desde request postman: " + username);
         }
-
 
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
 
         return authenticationManager.authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        //Obtenemos los roles
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+        //Escribimos los roles en el claims pero con tipo JSON
+        Claims claims = Jwts.claims();
+        claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
+
+        // Generando nuestro JWT
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(authResult.getName())
+                .signWith(SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000 * 4))
+                .compact();
+
+        //Declaramos el header para el response
+        response.addHeader("Authorization", "Bearer " + token);// Esto es un estandar de JWT
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("token", token);
+        body.put("user", (User) authResult.getPrincipal());
+        body.put("message", "Hola usuario, has iniciado sesión!");
+
+        //Escribimos el body en el response sin antes convertirlo a JSON con ObjectMapper
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
+
     }
 
 }
